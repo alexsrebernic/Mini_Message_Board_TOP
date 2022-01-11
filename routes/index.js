317df3f9683097b73ff5dbfd3bf09ea0a1437a51
Alex Srebernic <alexsrebernic@pop-os.localdomain>
2022-01-11 18:20:49 -0300

    final commits

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  },
-  {
-    text: "What's Up!",
-    user: "Alex",
-    added: new Date()
-  },
-  {
-    text: "Oh, hi!",
-    user: "Jack",
-    added: new Date()
-  }
-];
+const messages = require("./messages.js")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini messageboard',messages:messages });
 });
-router.get('/new',function(req,res,nect){
-  res.render('form')
-})
+
 router.post('/new',function(req,res,next) {
-  const bodyRequest = req.body
-  console.log(bodyRequest)
+  const newMessage = {
+    text: req.body.text, 
+    user: req.body.user, 
+    details: req.body.details,
+    added: new Date()
+  }
+  messages.unshift(newMessage);
+  res.redirect('/')
+})
+router.get('/new', (req, res, next) => {
+  res.render('form');
 })
 module.exports = router;
---
+++ routes/messages.js
@@ -0,0 +1,27 @@
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      details:'Today i learned something new',
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      details:"I'm feeling great today",
+      added: new Date()
+    },
+    {
+      text: "What's Up!",
+      user: "Alex",
+      details:'...',
+      added: new Date()
+    },
+    {
+      text: "Oh, hi!",
+      user: "Jack",
+      details:'Hmm nothing to say',
+      added: new Date()
+    }
+  ];
+module.exports = messages
